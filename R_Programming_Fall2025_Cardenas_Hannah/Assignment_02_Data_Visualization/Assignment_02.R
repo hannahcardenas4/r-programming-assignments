5af1c40b13ec9f0fe90488a8c11668b2e36eef59
Hannah Cardenas <135150895+hannahcardenas4@users.noreply.github.com>
2025-09-14 10:07:01 -0400

    Update Assignment_02.R to reflect new folder structure

--- R_Programming_Fall2025_Cardenas_Hannah/Assignment_02_Data_Visualization/Assignment_02.R
+++ R_Programming_Fall2025_Cardenas_Hannah/Assignment_02_Data_Visualization/Assignment_02.R
@@ -1,1 +1,13 @@
---
+## Stored vector
+assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+
+## Confirmation vector was stored successfully
+assignment2
+
+## Corrected myMean function
+myMean <- function(assignment2) {
+  return(sum(assignment2) / length(assignment2))
+}
+
+## Mean value
+myMean(assignment2)
