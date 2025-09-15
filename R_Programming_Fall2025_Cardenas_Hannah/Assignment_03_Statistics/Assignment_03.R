075fb971fb0c54e3190ee7c43cb9d3cd6db13dd9
Hannah Cardenas <135150895+hannahcardenas4@users.noreply.github.com>
2025-09-14 20:57:01 -0400

    Update Assignment_03.R to include plot

--- R_Programming_Fall2025_Cardenas_Hannah/Assignment_03_Statistics/Assignment_03.R
+++ R_Programming_Fall2025_Cardenas_Hannah/Assignment_03_Statistics/Assignment_03.R
@@ -15,10 +15,27 @@ mean(df_polls$ABC_poll)
 mean(df_polls$CBS_poll)
 median(df_polls$ABC_poll)
 median(df_polls$CBS_poll)
-range(df_polls[, c("ABC_poll","CBS_poll")])
+range(df_polls$ABC_poll)
+range(df_polls$CBS_poll)
 
 ## Add a column for difference between CBS and ABC
-df_polls$Diff <- df_polls$CBS_poll - df_polls$ABC_pol
+df_polls$Diff <- df_polls$CBS_poll - df_polls$ABC_poll
 
 ## View new data frame 
 head(df_polls)
+
+## Load ggplot2
+library(ggplot2)
+
+## Generate plot
+df_polls$Name <- factor(df_polls$Name, levels = df_polls$Name)
+
+plot <- ggplot(df_polls, aes(x = Name, y = Diff)) +
+  geom_col() +
+  labs(title = "CBS vs ABC Poll Difference by Candidate",
+       x = "Candidate",
+       y = "CBS - ABC (percentage points)") +
+  theme_minimal(base_size = 12)
+
+## Print plot
+print(plot)
