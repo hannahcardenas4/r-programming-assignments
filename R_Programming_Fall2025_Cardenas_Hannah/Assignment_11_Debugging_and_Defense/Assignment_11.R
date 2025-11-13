371b5d917f996d9a2591b0bb50667fb50f2d3298
Hannah Cardenas <135150895+hannahcardenas4@users.noreply.github.com>
2025-11-13 07:29:59 -0500

    Create Assignment_11.R

---
+++ R_Programming_Fall2025_Cardenas_Hannah/Assignment_11_Debugging_and_Defense/Assignment_11.R
@@ -0,0 +1,21 @@
+tukey.outlier <- function(x) {
+  q <- quantile(x, c(0.25, 0.75))
+  H <- 1.5 * IQR(x)
+  x < (q[1] - H) | x > (q[2] + H)
+}
+
+corrected_tukey <- function(x) {
+  outliers <- array(TRUE, dim = dim(x))
+  for (j in seq_len(ncol(x))) {
+    outliers[, j] <- outliers[, j] & tukey.outlier(x[, j])
+  }
+  outlier.vec <- logical(nrow(x))
+  for (i in seq_len(nrow(x))) {
+    outlier.vec[i] <- all(outliers[i, ])
+  }
+  outlier.vec
+}
+
+set.seed(123)
+test_mat <- matrix(rnorm(50), nrow = 10)
+corrected_tukey(test_mat)
