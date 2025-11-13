f08b14d9785a003a098e321a81c88563f325d309
Hannah Cardenas <135150895+hannahcardenas4@users.noreply.github.com>
2025-11-13 07:36:57 -0500

    Update Assignment_11.R with small commenting changes

--- R_Programming_Fall2025_Cardenas_Hannah/Assignment_11_Debugging_and_Defense/Assignment_11.R
+++ R_Programming_Fall2025_Cardenas_Hannah/Assignment_11_Debugging_and_Defense/Assignment_11.R
@@ -1,9 +1,11 @@
+# Helper function
 tukey.outlier <- function(x) {
   q <- quantile(x, c(0.25, 0.75))
   H <- 1.5 * IQR(x)
   x < (q[1] - H) | x > (q[2] + H)
 }
 
+# Corrected Tukey-multiple function
 corrected_tukey <- function(x) {
   outliers <- array(TRUE, dim = dim(x))
   for (j in seq_len(ncol(x))) {
